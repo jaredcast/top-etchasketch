e8f654bd17cd6608f956404c61ca307831afc151
jaredcast <jaredcast97@gmail.com>
2023-09-12 15:07:40 -0700

    Made a reset button, fixed the change size button to properly make the input empty on a new grid

--- funcs.js
+++ funcs.js
@@ -1,34 +1,68 @@
 const grid = document.querySelector('.grid');
 const sizeBtn = document.querySelector('.sizeBtn');
-
-//Get the size from input and create a new grid for it
+const reset = document.querySelector('.reset');
+const sizeDim = document.querySelector('.sizeDim');
+//Change size button - Get the size from input and create a new grid for it
 sizeBtn.addEventListener('click', () => {
-    //const sizeInput = document.querySelector('#size').value
-    let sizeInput = document.getElementById('size').value;
-    createGrid(sizeInput, sizeInput);
-    sizeInput = "";
+    let sizeInput = document.querySelector('#size').value
+    console.log(sizeInput);
+    createGrid(document.querySelector('#size').value);
+    //doesnt work
+    //sizeInput = "";
+    //works
+    sizeDim.innerHTML = `Size of Grid (${sizeInput} x ${sizeInput})`;
+    document.querySelector('#size').value = "";
 })
 
-let createGrid = (r, c) => {
-    if (r <= 0 || c <= 0) {
+//Reset Button - Removes the color styling upon reset.
+reset.addEventListener('click', () => {
+    const allBoxes = document.querySelectorAll(".box");
+    allBoxes.forEach((box) => {
+        //console.log(box);
+        box.style.backgroundColor = "";
+    })
+})
+
+let createGrid = (size) => {
+    if (size <= 0) {
         alert("ERROR! Can't have a number <= 0.")
         return;
     }
-    grid.innerHTML = "";
-    for (let i = 0; i < r; i++) {
-        const row = document.createElement('div');
-        row.className = 'row';
-        grid.appendChild(row);
-        for (let j = 0; j < c; j++) {
-            const square = document.createElement('div');
-            square.className = 'square';
-            square.addEventListener('mouseover', (event) =>{
-                event.target.style.backgroundColor = "black";
-            })
-            row.appendChild(square);
-        }
+    if (size > 100) {
+        alert("ERROR! Can't have a number > 100 .")
+        return;
     }
-    sizeBtn.reset();
+    
+    grid.innerHTML = "";
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    //Add a ton of squares
+    for (let i = 0; i < size*size; i++) {
+        const square = document.createElement('div');
+        square.className = 'box';
+        square.addEventListener('mouseover', (event) =>{
+            event.target.style.backgroundColor = "black";
+        })
+        grid.appendChild(square);
+    }
+    
+
+    //Old method - you can ignore this
+    // for (let i = 0; i < r; i++) {
+    //     const row = document.createElement('div');
+    //     row.className = 'row';
+    //     grid.appendChild(row);
+    //     for (let j = 0; j < c; j++) {
+    //         const square = document.createElement('div');
+    //         square.className = 'square';
+    //         square.addEventListener('mouseover', (event) =>{
+    //             event.target.style.backgroundColor = "black";
+    //         })
+    //         row.appendChild(square);
+    //     }
+    // }
+    // sizeBtn.reset();
 }
 
-createGrid(5,5);
+createGrid(16);
